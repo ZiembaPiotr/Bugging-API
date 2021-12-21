641aa6bfc86e6b3ac7834d391e6675ce4b0d27e7
ZiembaPiotr <p.ziemba99@gmial.com>
2021-12-21 15:31:57 +0100

    Setting Up Project Structure

---
+++ Entities/Reservation.go
@@ -0,0 +1,12 @@
+package Entities
+
+type Reservation struct {
+	HotelID      int    `json:"hotel_id"`
+	RoomID       int    `json:"room_id"`
+	GuestID      int    `json:"guest_id"`
+	CheckInDate  string `json:"check_in_date"`
+	CheckOutDate string `json:"check_out_date"`
+	OpinionID    int    `json:"opinion_id"`
+	Mark         int    `json:"mark"`
+	CreatedOn    string `json:"created_on"`
+}
